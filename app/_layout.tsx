786652ee75ff07005dbdbbc68e028584ae0b3b44
zhiheng-zhang-Mera <15601654187@163.com>
2026-04-04 16:03:43 +1100

    layout packing for collecting score

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,20 @@
 import { Stack } from 'expo-router';
+import { ExperimentProvider } from '../src/context/ExperimentContext';
 import { HealthProvider } from '../src/context/HealthContext';
-// 1. 导入你的 PrivacyProvider
 import { PrivacyProvider } from '../src/context/PrivacyContext';
 
 export default function RootLayout() {
   return (
-    // 2. 将 PrivacyProvider 套在最外层
-    <PrivacyProvider>
-      <HealthProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        </Stack>
-      </HealthProvider>
-    </PrivacyProvider>
+    // 2. 将 ExperimentProvider 套在最外层，确保全局可收集打分和耗时数据
+    <ExperimentProvider>
+      <PrivacyProvider>
+        <HealthProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          </Stack>
+        </HealthProvider>
+      </PrivacyProvider>
+    </ExperimentProvider>
   );
 }
