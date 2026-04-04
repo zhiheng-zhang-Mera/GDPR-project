3975e75b34548bdf138a02404a51df84271dee78
zhiheng-zhang-Mera <15601654187@163.com>
2026-04-04 16:08:27 +1100

    link packages manually

--- android/app/src/main/java/com/anonymous/mymobileapp/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/mymobileapp/MainApplication.kt
@@ -16,6 +16,12 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
+import com.facebook.react.defaults.DefaultReactNativeHost
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
+
+import com.anonymous.mymobileapp.privacy.PrivacyInterceptorPackage
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
@@ -24,7 +30,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              add(PrivacyInterceptorPackage())
             }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
